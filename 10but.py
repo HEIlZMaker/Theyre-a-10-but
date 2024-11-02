3098bb015301aa1c446ff8833b45cca18fcaf883
heilzubu <alexander.lindin@gmail.com>
2024-11-02 10:22:20 +0100

    first commit2

---
+++ 10but.py
@@ -0,0 +1,29 @@
+import random
+import time 
+duplicateset = set(())
+player = ""
+
+while player != 'q':
+    epoch_time = int(time.time())
+    random.seed(epoch_time)
+    block = random.randint(1,4)
+
+    if block == 1:
+        max = 60
+    elif block == 2:
+        max = 60
+    elif block == 3:
+        max = 47
+    elif block == 4:
+        max = 11
+    
+    question = random.randint(1, max)
+    result = block, question
+
+    if result not in duplicateset:
+        duplicateset.add(result)
+        print(block)
+        print(question)
+        print("press q to exit, any other key to continue")
+        player = input()
+#thank you darq for helping me :)
